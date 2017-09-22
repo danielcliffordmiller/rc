a13d3d8907e318d02f7ad00953a21cce72df1273
Dan Miller <danielcliffordmiller@gmail.com>
2017-09-21 23:15:12 -0500

    install.sh checks for previous installs

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo "source $(pwd)/bashrc" >> ~/.bashrc
-echo "source $(pwd)/vimrc" >> ~/.vimrc
-echo "source $(pwd)/tmux.conf" >> ~/.tmux.conf
-echo '$include' "$(pwd)/inputrc" >> ~/.inputrc
+if [ ! -e ~/.bashrc ] || ! grep -q -e "source $(pwd)/bashrc" ~/.bashrc; then
+	echo "source $(pwd)/bashrc" >> ~/.bashrc
+else
+	echo "~/.bashrc is already installed, skipping..."
+fi
+if [ ! -e ~/.vimrc ] || ! grep -q -e "source $(pwd)/vimrc" ~/.vimrc; then
+	echo "source $(pwd)/vimrc" >> ~/.vimrc
+else
+	echo "~/.vimrc is already installed, skipping..."
+fi
+if [ ! -e ~/.tmux.conf ] || ! grep -q -e "source $(pwd)/tmux.conf" ~/.tmux.conf; then
+	echo "source $(pwd)/tmux.conf" >> ~/.tmux.conf
+else
+	echo "~/.tmux.conf is already installed, skipping..."
+fi
+if [ ! -e ~/.inputrc ] || ! grep -q -e "\$include $(pwd)/inputrc" ~/.inputrc; then
+	echo '$include' "$(pwd)/inputrc" >> ~/.inputrc
+else
+	echo "~/.inputrc is already installed, skipping..."
+fi
