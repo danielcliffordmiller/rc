44e7e595b652ce12e2f092e571dd7a13b1ea65e6
Dan Miller <Daniel.C.Miller@target.com>
2017-10-25 15:02:20 -0500

    added reverse video for current day

--- cal3.pl
+++ cal3.pl
@@ -3,24 +3,30 @@
 use strict;
 use v5.12;
 
-my (undef, undef, undef, undef, $mon, $yr) = localtime;
+my (undef, undef, undef, $cur_day, $cur_month, $cur_year) = localtime;
 
-$mon = $ARGV[0] || $mon + 1;
-$yr = $ARGV[1] || $yr + 1900;
+$cur_month += 1;
+$cur_year += 1900;
 
 # at this point, the month and the year are absolute
+
+my ($rev, $off) = (`tput rev`, `tput sgr0`);
+
+my $mon = $ARGV[0] || $cur_month;
+my $yr = $ARGV[1] || $cur_year;
 
 die "invalid month '$mon'" if $mon < 1 || $mon > 12;
 
 my @cals = map {
-	[ `cal $_->[0], $_->[1]` ]
+	my @cal = qx( cal $_->[0], $_->[1] );
+	map{s/$cur_day/$rev$cur_day$off/} @cal if $_->[0] == $cur_month && $_->[1] == $cur_year;
+	foreach my $l (@cal) { chomp($l) }
+	[ @cal ];
 } (
 	$mon == 1 ? [ 12, $yr - 1 ] : [ $mon - 1, $yr ], 
 	[ $mon, $yr ],
 	$mon == 12 ? [ 1, $yr + 1 ] : [ $mon + 1, $yr ],
 );
-
-chomp @$_ foreach (@cals);
 
 my @lines;
 for(my $i=0; $i < scalar @cals; $i++) {
