c892a29423dc774465e160a81084c27f04a3ae48
Dan Miller <Daniel.C.Miller@target.com>
2018-01-29 14:36:41 -0600

    fixed tabs & spacing issues

--- cal3.pl
+++ cal3.pl
@@ -18,27 +18,25 @@ my $yr = $ARGV[1] || $cur_year;
 die "invalid month '$mon'" if $mon < 1 || $mon > 12;
 
 my @cals = map {
-	my @cal = qx( cal $_->[0], $_->[1] );
-	map{s/$cur_day/$rev$cur_day$off/ unless /$cur_year/} @cal if $_->[0] == $cur_month && $_->[1] == $cur_year;
-	foreach my $l (@cal) { chomp($l) }
-	[ @cal ];
+    my @cal = qx( cal $_->[0], $_->[1] );
+    foreach my $l (@cal) {
+	chomp($l); $l .= ' 'x(21 - length($l));
+    }
+    map{s/$cur_day/$rev$cur_day$off/ unless /$cur_year/} @cal if $_->[0] == $cur_month && $_->[1] == $cur_year;
+    [ @cal ];
 } (
-	$mon == 1 ? [ 12, $yr - 1 ] : [ $mon - 1, $yr ], 
-	[ $mon, $yr ],
-	$mon == 12 ? [ 1, $yr + 1 ] : [ $mon + 1, $yr ],
+    $mon == 1 ? [ 12, $yr - 1 ] : [ $mon - 1, $yr ],
+    [ $mon, $yr ],
+    $mon == 12 ? [ 1, $yr + 1 ] : [ $mon + 1, $yr ],
 );
 
 my @lines;
 for(my $i=0; $i < scalar @cals; $i++) {
-	for(my $j=0; $j < scalar @{$cals[$i]}; $j++) {
-		$lines[$j][$i] = $cals[$i][$j];
-	}
+    for(my $j=0; $j < scalar @{$cals[$i]}; $j++) {
+	$lines[$j][$i] = $cals[$i][$j];
+    }
 }
 
 foreach my $line (@lines) {
-	say join '', map {
-		my $r = " ".sprintf("%-21s", $_);
-		$r .= " " if index($r, $rev) > -1;
-		$r
-	} @$line;
+    say join '', map { ' '.$_ } @$line;
 }
